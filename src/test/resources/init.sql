292af73c244036f7a8a1ab05c1c5bf78401f0004
nromanen <nromanen.softserveinc@gmail.com>
2024-02-14 20:09:48 +0200

    Update init.sql

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -1,7 +1,7 @@
-create table categories (id bigserial primary key, avatar varchar not null, title varchar not null);
-create table club (id bigserial primary key,title varchar not null,description varchar not null,image_url varchar,category_id int8 references categories(id));
-create table child (id bigserial primary key,first_name varchar not null,last_name varchar not null,birth_date date);
-create table club_child(club_id int8 references club(id),child_id int8 references child(id));
+-- create table categories (id bigserial primary key, avatar varchar not null, title varchar not null);
+-- create table club (id bigserial primary key,title varchar not null,description varchar not null,image_url varchar,category_id int8 references categories(id));
+-- create table child (id bigserial primary key,first_name varchar not null,last_name varchar not null,birth_date date);
+-- create table club_child(club_id int8 references club(id),child_id int8 references child(id));
 --    truncate table club, categories restart identity;
 -- truncate table categories cascade;
 -- TRUNCATE TABLE table_foo, table_bar RESTART IDENTITY;
