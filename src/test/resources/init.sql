cbe093f26eb7e42d9fc95f2e8ddc0e7247eb6c24
nataliia <nromanen.softserveinc@gmail.com>
2024-02-14 19:31:53 +0200

    change init.sql with created database structure

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -1,4 +1,8 @@
-truncate table club, categories restart identity;
+create table categories (id bigserial primary key, avatar varchar not null, title varchar not null);
+create table club (id bigserial primary key,title varchar not null,description varchar not null,image_url varchar,category_id int8 references categories(id));
+create table child (id bigserial primary key,first_name varchar not null,last_name varchar not null,birth_date date);
+create table club_child(club_id int8 references club(id),child_id int8 references child(id));
+--    truncate table club, categories restart identity;
 -- truncate table categories cascade;
 -- TRUNCATE TABLE table_foo, table_bar RESTART IDENTITY;
 insert into categories (title, avatar) values ('Old Category', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII');
